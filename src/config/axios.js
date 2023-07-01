0f1666bc807cbc5ee1c6f88de10a92698fbb8bda
yuyasano0096 <mackbus0096@gmail.com>
2023-07-01 05:31:11 +0000

    s

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const token = localStorage.getItem("token")
 
 const clienteAxios = axios.create({
-    baseURL:'http://localhost:4000/v1/',
+    baseURL:'http://206.189.239.87:4000/v1/',
     headers: {
         'x-token': token
       }
